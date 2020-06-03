c01a69b20b3d8b5d27cabb0dfd2e1ae0920228c4
Marco Antonio V Montoya Cardenas <marcovmontoya@gmail.com>
2020-06-03 16:23:35 -0400

    feat: add unique style test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import styled from "styled-components";
 import './App.css';
 import {CardComponent} from "./components/card/card.component";
+
+const Text = styled.div`
+ color: red;
+ font-size: 29px;
+ border: ${({isActive}) => isActive ? '1px solid black' : '3px dotted green'};
+`;
 
 function App() {
   return (
     <div className="App">
       <CardComponent>
-        <div>
-          I am a component.
-        </div>
+        <Text isActive={true}>I am a component.</Text>
       </CardComponent>
     </div>
   );
